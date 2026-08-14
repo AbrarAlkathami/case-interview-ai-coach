d043ce5c837d7ca98dc9789077181ddf939e1e04
AbrarAlkathami <AbrarAlkathami@gmail.com>
2026-08-14 16:25:27 +0300

    chore: create generic crud for our models

---
+++ src/crud/base.py
@@ -0,0 +1,44 @@
+from typing import Generic, TypeVar, Type
+from sqlalchemy.orm import Session
+
+from db.base import Base
+
+
+ModelType = TypeVar("ModelType", bound=Base)
+
+
+class CRUDBase(Generic[ModelType]):
+
+    def __init__(self, model: Type[ModelType]):
+        self.model = model
+
+
+    def get(self, db: Session, id: int):
+        return db.get(self.model, id)
+
+    def get_all(self, db: Session):
+        return db.query(self.model).all()
+
+    def create(self, db: Session, data: dict):
+        obj = self.model(**data)
+        db.add(obj)
+        db.commit()
+        db.refresh(obj)
+        return obj
+
+    def update(self, db: Session, obj, data: dict):
+        for key, value in data.items():
+            setattr(obj, key, value)
+
+        db.commit()
+        db.refresh(obj)
+        return obj
+
+    def delete(self, db: Session, id: int):
+        obj = db.get(self.model, id)
+
+        if obj:
+            db.delete(obj)
+            db.commit()
+
+        return obj
