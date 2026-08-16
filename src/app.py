d20ce02e5dc1186fdca65bc6508d4ccd36f7f84c
AbrarAlkathami <AbrarAlkathami@gmail.com>
2026-08-16 18:35:06 +0300

    chore: add app middleware

--- src/app.py
+++ src/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from .routers import user, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+origins = [
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(auth.router)
